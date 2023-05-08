25a2a39e95af465e46a184a19b6415747f15b5e9
pirnataly <piroznatal@gmail.com>
2023-05-08 20:40:21 +0300

    feat: add solution for sixth task

---
+++ 06-build-page/index.js
@@ -0,0 +1,103 @@
+const fsPromise = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+const stylesPath = path.resolve(__dirname, 'styles');
+const stylePath = path.resolve(__dirname, 'project-dist', 'style.css');
+const templatePath = path.resolve(__dirname, 'template.html');
+const componentsDirPath = path.resolve(__dirname, 'components');
+const indexPath = path.resolve(__dirname, 'project-dist', 'index.html');
+
+async function start() {
+    await createDir();
+    await compileCss();
+    await copyAssets();
+    await buildHtml();
+}
+
+
+async function createDir() {
+    await fsPromise.mkdir(path.resolve(__dirname, 'project-dist'), {recursive: true});
+
+}
+
+async function copyAssets() {
+    await fsPromise.cp(path.resolve(__dirname, 'assets'), path.resolve(__dirname, 'project-dist', 'assets'), {recursive: true});
+}
+
+async function replaceAsync(str, regex, asyncFn) {
+    const promises = [];
+    str.replace(regex, (match, ...args) => {
+        const promise = asyncFn(match, ...args);
+        promises.push(promise);
+    });
+    const data = await Promise.all(promises);
+    return str.replace(regex, () => data.shift());
+}
+
+async function buildHtml() {
+    const text = await read(templatePath);
+    const replacedString = await replaceAsync(text, /{{(.*?)}}/gi, changeText)
+    await save(indexPath, replacedString);
+
+}
+
+async function changeText(match) {
+
+    const word = match.replace('{{', '').replace('}}', '');
+    const files = await fsPromise.readdir(componentsDirPath, {withFileTypes: true});
+    for (const file of files) {
+        if (file.isFile() && (path.extname(file.name).slice(1) === 'html' && path.parse(file.name).name) === word) {
+
+            const filePath = path.resolve(componentsDirPath, file.name);
+            const newtext = await read(filePath);
+            console.log(newtext);
+
+            return newtext;
+        }
+
+
+    }
+}
+
+
+const arr = [];
+
+function read(filePath) {
+    return new Promise(resolve => {
+
+        const readableStream = fs.createReadStream(filePath, 'utf-8');
+        readableStream.on('data', chunk => {
+            resolve(chunk)
+        });
+    })
+
+
+}
+
+
+async function save(pathFile, data) {
+    fsPromise.writeFile(
+        pathFile,
+        data,
+        (err) => {
+            if (err) throw err;
+        }
+    )
+}
+
+
+async function compileCss() {
+    const files = await fsPromise.readdir(stylesPath, {withFileTypes: true});
+    for (const file of files) {
+        if (file.isFile() && (path.extname(file.name).slice(1) === 'css')) {
+            const filePath = path.resolve(stylesPath, file.name)
+            const text = await read(filePath);
+            arr.push(text)
+        }
+        await save(stylePath, arr);
+
+    }
+}
+
+start();
